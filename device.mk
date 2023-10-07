6dca05d4ef21076aba2c6ce81dfccac7ea2e840f
LuK1337 <priv.luk@gmail.com>
2023-10-07 16:44:47 +0200

    X6837: Set default Settings.Global.DEVICE_NAME according to XOS
    XOS uses ro.product.product.tran.device.name.default for default device name.
    
    Change-Id: I310c1ffc581b72653783d1093d9e7ebde544bb16
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -278,6 +278,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworksResTarget \
+    SettingsProviderResTarget \
     SystemUIResTarget \
     TelephonyResTarget
 
