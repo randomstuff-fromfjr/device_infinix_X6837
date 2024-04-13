1952def27d964cc398b3096c4d02c14a108e17fc
fjrXTR <fajarslebew31@gmail.com>
2024-04-13 16:34:14 +0000

    X6837: Initial tree * taken from aospdtgen
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_X6837.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_X6837-user \
+    lineage_X6837-userdebug \
+    lineage_X6837-eng
---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for Infinix HOT 40 Pro (X6837)
+
+```
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
