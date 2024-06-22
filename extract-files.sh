af17bdb62f9dfd7593f8532e1fa071e535b59bf0
ChrisCatto <chriscatto08@gmail.com>
2024-06-22 16:29:50 +0200

    X6837: Link mediatek camera provider against v32 `libutils`
    * Abort message: 'incStrongRequireStrong() called on 0xb40000770d0587a0
      which isn't already owned'
    * Required after U QPR3
    
    Change-Id: Ie6ea7f331207f604cb87804c67f4de9fd9a60d76
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -87,6 +87,9 @@ function blob_fixup() {
             "${PATCHELF}" --replace-needed "android.hardware.security.sharedsecret-V1-ndk_platform.so" "android.hardware.security.sharedsecret-V1-ndk.so" "${2}"
             grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
+        vendor/lib64/hw/mt6789/android.hardware.camera.provider@2.6-impl-mediatek.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            ;;
     esac
 }
 
