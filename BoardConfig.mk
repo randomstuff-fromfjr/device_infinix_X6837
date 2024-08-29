X6837/android_device_infinix_X6837

X6837: Add recovery init scripts and required flags
Signed-off-by: Woomymy <woomy@woomy.be>
Change-Id: I6a3b4ba0037d9fec49e8445ed8e615b2d14e8c7f
Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

BoardConfig.mk
@@ -106,5 +106,11 @@ TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
 TARGET_SYSTEM_PROP += $(CONFIGS_PATH)/configs/properties/system.prop
 TARGET_VENDOR_PROP += $(CONFIGS_PATH)/configs/properties/vendor.prop
 
+# Recovery
+BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.mt6789
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USERIMAGES_USE_F2FS := true
+
 # Inherit the proprietary files
 include vendor/infinix/X6837/BoardConfigVendor.mk

randomstuff-fromfjr/device_infinix_X6837

Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

BoardConfig.mk
@@ -98,5 +98,11 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_BOARD_PLATFORM := mt6789
 BOARD_HAS_MTK_HARDWARE := true
 
+# Recovery
+BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6789
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USERIMAGES_USE_F2FS := true
+
 # Inherit the proprietary files
 include vendor/infinix/X6837/BoardConfigVendor.mk