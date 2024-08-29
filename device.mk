X6837/android_device_infinix_X6837

X6837: Build AIDL light hal
Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

device.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
     android.hardware.security.sharedsecret-V1-ndk_platform.vendor \
     libcppbor_external.vendor:64
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.infinix
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor:64

randomstuff-fromfjr/device_infinix_X6837

Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

device.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
     android.hardware.security.sharedsecret-V1-ndk_platform.vendor \
     libcppbor_external.vendor:64
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.transsion
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \