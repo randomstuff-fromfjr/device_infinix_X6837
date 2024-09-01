0f7385bbdd4ba0c4502d1f732240a1acf88b33ea
fjrXTR <fajarslebew31@gmail.com>
2024-09-01 05:27:11 +0000

    X6837: overlay: Import IMS required overlay
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -278,7 +278,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworksResTarget \
-    SystemUIResTarget
+    SystemUIResTarget \
+    TelephonyResTarget
 
 # Permissions
 PRODUCT_COPY_FILES += \
