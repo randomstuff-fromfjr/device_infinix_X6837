X6837/android_device_infinix_X6837

X6837: Build transsion touch hidl
Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

BoardConfig.mk
@@ -131,7 +131,7 @@ BOARD_HAS_MTK_HARDWARE := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := /proc/gesture_function
-TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
@@ -141,7 +141,7 @@ TARGET_VENDOR_PROP += $(CONFIGS_PATH)/configs/properties/vendor.prop
 # Recovery
 BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
-TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.mt6789
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6789
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_F2FS := true
 
@@ -150,6 +150,11 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
+# Touch
+SOONG_CONFIG_NAMESPACES += TRANSSION_TOUCH
+SOONG_CONFIG_TRANSSION_TOUCH := HIGH_TOUCH_POLLING_PATH
+SOONG_CONFIG_TRANSSION_TOUCH_HIGH_TOUCH_POLLING_PATH := /proc/game_state
 
 # Vendor Security Patch
 VENDOR_SECURITY_PATCH := 2024-05-05
@@ -181,9 +186,11 @@ BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
 BOARD_VNDK_VERSION := current
 
 # VINTF
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+	$(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
+	vendor/lineage/config/device_framework_matrix.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
 # Wi-Fi
 WPA_SUPPLICANT_VERSION := VER_0_8_X

randomstuff-fromfjr/device_infinix_X6837

Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

BoardConfig.mk
@@ -152,6 +152,11 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := X6837,Infinix-X6837
 
+# Touch
+SOONG_CONFIG_NAMESPACES += TRANSSION_TOUCH
+SOONG_CONFIG_TRANSSION_TOUCH := HIGH_TOUCH_POLLING_PATH
+SOONG_CONFIG_TRANSSION_TOUCH_HIGH_TOUCH_POLLING_PATH := /proc/game_state
+
 # Vendor Security Patch
 VENDOR_SECURITY_PATCH := 2024-03-05
 
@@ -182,7 +187,9 @@ BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
 BOARD_VNDK_VERSION := current
 
 # VINTF
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(CONFIGS_PATH)/vintf/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+	$(CONFIGS_PATH)/configs/vintf/framework_compatibility_matrix.xml \
+	vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(CONFIGS_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(CONFIGS_PATH)/vintf/compatibility_matrix.xml
 