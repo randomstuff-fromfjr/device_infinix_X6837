X6837/android_device_infinix_X6837

X6837: Add power HAL extension and use xiaomi-touch to handle dt2w
Change-Id: I5c65e0df255853eb2b3018a27ceb807c111bc4df
Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

BoardConfig.mk
@@ -114,6 +114,9 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 TARGET_BOARD_PLATFORM := mt6789
 BOARD_HAS_MTK_HARDWARE := true
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
 TARGET_SYSTEM_PROP += $(CONFIGS_PATH)/configs/properties/system.prop

randomstuff-fromfjr/device_infinix_X6837

Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

BoardConfig.mk
@@ -131,6 +131,9 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 TARGET_BOARD_PLATFORM := mt6789
 BOARD_HAS_MTK_HARDWARE := true
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_VENDOR_PROP += $(CONFIGS_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(CONFIGS_PATH)/properties/vendor.prop