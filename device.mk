7f02158b45a7ec133589cdf38cf3425be3c178e8
fjrXTR <fajarslebew31@gmail.com>
2024-09-01 15:11:41 +0000

    X6837: Build libwifi-hal-wrapper
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -476,17 +476,18 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
+    libwifi-hal-wrapper \
     android.hardware.wifi-service \
     wpa_supplicant \
     hostapd \
-	libkeystore-wifi-hidl:64 \
+    libkeystore-wifi-hidl:64 \
     libkeystore-engine-wifi-hidl:64
-
+    
 PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor
-
+    
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
