1e80f7498fa445e72fa6235c6a031825b1ef1056
fjrXTR <fajarslebew31@gmail.com>
2024-10-06 13:10:15 +0000

    X6837: Fix Maintainer name on Rising's Settings * Make it signed too
    Change-Id: I2beb0e56b43d2d8451a964d6a53ae378bb4d67df
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- lineage_X6837.mk
+++ lineage_X6837.mk
@@ -10,14 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common RisingOSS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-RISING_CHIPSET="Helio G99"
-RISING_MAINTAINER="gabutuniverse!
 TARGET_ENABLE_BLUR := true
 PRODUCT_NO_CAMERA := false
 TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true 
 WITH_GMS := true
 TARGET_CORE_GMS := true
 TARGET_CORE_GMS_EXTRAS := true
+-include vendor/lineage-priv/keys/keys.mk
 
 # Inherit from X6837 device
 $(call inherit-product, device/infinix/X6837/device.mk)
@@ -33,9 +32,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-transsion
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sys_tssi_64_armv82_infinix-user 13 TP1A.220624.014 526124 release-keys" \
-    PRODUCT_NAME=X6837-OP
+    PRODUCT_NAME=X6837-OP \
+    RISING_CHIPSET="Helio G99" \
+    RISING_MAINTAINER="gabutuniverse! (fjrXTR)"
 
 BUILD_FINGERPRINT := Infinix/X6837-OP/Infinix-X6837:13/TP1A.220624.014/240313V1154:user/release-keys
-
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
