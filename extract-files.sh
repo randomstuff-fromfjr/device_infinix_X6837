9290eb9263309e0fd1fbe86f136fbe74f7249cc2
fjrXTR <fajarslebew31@gmail.com>
2024-10-08 12:43:11 +0000

    X6837: Switch to 32-bit Media services * trying to fix Alight motion issue
    Change-Id: Ibde9877ada232b3258fb35ed6939c447ea5a18b3
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -61,10 +61,7 @@ function blob_fixup() {
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
-        vendor/etc/init/android.hardware.media.c2@1.2-mediatek.rc)
-            sed -i 's/@1.2-mediatek/@1.2-mediatek-64b/g' "${2}"
-            ;;
-        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
+        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
             "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
             "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
